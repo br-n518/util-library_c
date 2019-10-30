8270f5b3ac5aaf21fa05ab46f0e6b2a8f1dbc2cd
br-n518 <brn518@example.com>
2019-10-30 14:08:25 -0700

    Add convenience header for including all headers.

---
+++ src/util/all.h
@@ -0,0 +1,18 @@
+#ifndef UTIL_LIB_ALL_HEADERS_H
+#define UTIL_LIB_ALL_HEADERS_H
+
+
+#include "btree.h"
+#include "grammar.h"
+#include "graph.h"
+#include "hash.h"
+#include "hash_e.h"
+#include "node.h"
+#include "strbuff.h"
+#include "vector_str.h"
+
+#include "ini_doc.h"
+#include "xml_doc.h"
+
+#endif
+
